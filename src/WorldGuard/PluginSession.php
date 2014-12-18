a846c2c08c4af81f7945eee3611d1c1915a969cf
Hmy2001 <harutowii@yahoo.co.jp>
2014-12-19 01:26:11 +0900

    add PluginSession.php and WorldGuard.php, write

---
+++ src/WorldGuard/PluginSession.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WorldGuard;
+
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+//use pocketmine\utils\Config;
+
+use WorldEditor\WorldEditor;
+use WEdit\WEdit;
+
+class PluginSession{
+
+public function __construct($dataFolder){
+$this->DataFolder = $dataFolder;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/WorldGuard/WorldGuard.php
+++ src/WorldGuard/WorldGuard.php
@@ -1,3 +1,55 @@
 <?php
 
 namespace WorldGuard;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\block\Block;
+use pocketmine\item\Item;
+
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+
+class WorldGuard extends PluginBase implements Listener, CommandExecutor{
+
+	public function onEnable(){//Œã‚Å–|–ó
+		$this->getLogger()->info("WorldGuard loaded!");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+                @mkdir($this->getDataFolder());
+		//$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, array());
+		//$this->config->save();
+                //$this->dbManager = new WorldGuardDatabaseManager($this->getDataFolder());
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public function onDisable(){
+                //save function
+		$this->getLogger()->info("WorldGuard was to save the area information!");
+	}
+
+}

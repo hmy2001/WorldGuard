0ab46975e4157d81be88e56129463417eef44d54
Hmy2001 <harutowii@yahoo.co.jp>
2014-12-19 18:10:42 +0900

    英文翻訳

--- src/WorldGuard/WorldGuard.php
+++ src/WorldGuard/WorldGuard.php
@@ -35,35 +35,49 @@ class WorldGuard extends PluginBase implements Listener, CommandExecutor{
 	        $this->dbManager = new WorldGuardDatabaseManager($this->getDataFolder());
 	}
 
-	public function onCommand(CommandSender $sender, Command $command, $label, array $args){//後で翻訳
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         	$username = $sender->getName();
                 if(!($sender instanceof Player)){
-                $sender->sendMessage("Please run this command in-game.");
+                $sender->sendMessage("このコマンドはゲーム内で実行してください。");
                 return true;
                 break;
                 }
         	switch($command->getName()){
        		case "/region":
                 	if(!isset($args[0]) and $args[0] === ""){
-                                $sender->sendMessage("usage: //region claim [Name]\n".
-                                                     "usage: //region remove [Name]\n".
-                                                     "usage: //region info [Name]\n".
-                                                     "usage: //region select [Name]\n".
-                                                     "usage: //region list [PageName]"
+                                $sender->sendMessage("使用法: //region claim [名前]\n".
+                                                     "使用法: //region remove [名前]\n".
+                                                     "使用法: //region info [名前]\n".
+                                                     "使用法: //region select [名前]\n".
+                                                     "使用法: //region list [ページ番号]"
                                                      );
                 	return true;
                 	break;
                 	}
                 	switch($args[0]){
                 	case "claim":
-                		$sender->sendMessage("Test Message");
+				if(!isset($args[1]) or $args[1] === ""){
+				$sender->sendMessage("使用法: //region claim [名前]");
+				break;
+				}
+				if(!$this->dbManager->getName($sender,$name = $args[1])->fetchArray(SQLITE3_ASSOC)){
+					$sender->sendMessage("".$name."の名前のエリアは存在しています。");
+                                break;
+                                }
+                                $session = $this->pluginsession->getSession($sender)["selection"];
+                                if(!$session){
+                                        $this->WG_claim($sender,$name,$session);
+                                }else{
+                                	$sender->sendMessage("範囲指定がされておりません。");
+                                }
+                		//$sender->sendMessage("Test Message");
                 	break;
                         default:
-                                $sender->sendMessage("usage: //region claim [Name]\n".
-                                                     "usage: //region remove [Name]\n".
-                                                     "usage: //region info [Name]\n".
-                                                     "usage: //region select [Name]\n".
-                                                     "usage: //region list [PageName]"
+                                $sender->sendMessage("使用法: //region claim [名前]\n".
+                                                     "使用法: //region remove [名前]\n".
+                                                     "使用法: //region info [名前]\n".
+                                                     "使用法: //region select [名前]\n".
+                                                     "使用法: //region list [ページ番号]"
                                                      );
                         break;
                 	}
@@ -72,8 +86,22 @@ class WorldGuard extends PluginBase implements Listener, CommandExecutor{
                 return true;
 	}
 
+	public function WG_claim($player,$name,$session){
+		//TODO
+		
+		
+		
+		
+		
+		
+
+
+
+
+        }
+
 	public function onDisable(){
-                //save function
+                //save function TODO
 		$this->getLogger()->info("WorldGuardのエリア情報を保存しました。");
 	}
 
